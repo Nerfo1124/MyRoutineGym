d3abd270e7bb735b28ee2027e0fed375cd498711
Nerfo1124 <dfernando1124@hotmail.com>
2016-09-22 23:42:48 -0500

    Cuerpo de la aplicacion

--- app/src/main/java/co/com/nerfo/myroutinegym/view/GrupoDos.java
+++ app/src/main/java/co/com/nerfo/myroutinegym/view/GrupoDos.java
@@ -12,7 +12,6 @@ import android.widget.ListView;
 
 import java.util.List;
 
-import co.com.nerfo.myroutinegym.R;
 import co.com.nerfo.myroutinegym.dao.EjercicioDAO;
 import co.com.nerfo.myroutinegym.vo.EjercicioVO;
 
--- app/src/main/java/co/com/nerfo/myroutinegym/view/Principal.java
+++ app/src/main/java/co/com/nerfo/myroutinegym/view/Principal.java
@@ -1,9 +1,9 @@
 package co.com.nerfo.myroutinegym.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import co.com.nerfo.myroutinegym.R;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
 
@@ -11,5 +11,20 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void viewGrupoUno(View v){
+        Intent intent  = new Intent(this,GrupoUno.class);
+        startActivity(intent);
+    }
+
+    public void viewGrupoDos(View v){
+        Intent intent  = new Intent(this,GrupoDos.class);
+        startActivity(intent);
+    }
+
+    public void viewGrupoTres(View v){
+        Intent intent  = new Intent(this,GrupoTres.class);
+        startActivity(intent);
     }
 }
